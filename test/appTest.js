27e10cf0d3f47ea339d1bbc1de6f3641698b1bbc
AninHuang <lifeanne19904@gmail.com>
2019-10-18 23:24:55 +0800

    Another function for testing the addition

--- test/appTest.js
+++ test/appTest.js
@@ -1,14 +1,35 @@
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
+// const sayHello = require('../app').sayHello;
+// const addNumbers = require('../app').addNumbers;
+const app = require('../app');
+
+// Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5, 5);
 
 describe('App', function() {
-  it('app should return Hello', function() {
-    let result = sayHello;
-    assert.equal(result, 'Hello');
+  // Nested describe
+  describe('sayHello()', function() {
+    it('sayHello should return Hello', function() {
+      //let result = app.sayHello();
+      assert.equal(sayHelloResult, 'Hello');
+    });
+  
+    it('sayHello should return type string', function() {
+      //let result = app.sayHello();
+      assert.typeOf(sayHelloResult, 'string');
+    });
   });
 
-  it('sayHello should return type string', function() {
-    let result = sayHello;
-    assert.typeof(result, 'string');
+  describe('addNumbers()', function() {
+    it('addNumbers should be above 5', function() {
+      //let result = app.addNumbers(5, 5);
+      assert.isAbove(addNumbersResult, 5);
+    });
+  
+    it('addNumbers should return type number', function() {
+      //let result = app.addNumbers(5, 5);
+      assert.typeOf(addNumbersResult, 'number');
+    });
   });
 });
